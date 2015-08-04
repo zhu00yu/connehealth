b7ae8978ed94e576033be4d50ced828e3ab03afc
zhu00yu <zhu00yu@yahoo.com.cn>
2015-08-04 17:47:18 +0800

    For hsqldb

---
+++ src/main/resources/test_data/authorization_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO users VALUES ('admin', 'admin', '123', 1);
+INSERT INTO users VALUES ('user', 'user', '123', 1);
+
+
+INSERT INTO authorities VALUES ('admin', 'ROLE_ADMIN');
+INSERT INTO authorities VALUES ('admin', 'ROLE_USER');
+INSERT INTO authorities VALUES ('user', 'ROLE_USER');
